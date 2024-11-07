264d5ac97db06b5d96dc140d8783696d5d668630
duhnuker <dinukanh@gmail.com>
2024-11-07 23:10:46 +1100

    App Authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,43 @@
+import { useState, useEffect} from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import axios from "axios";
+import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 function App() {
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthenticated = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/auth/verify",
+        {},
+        {
+          headers: { jwt_token: localStorage.getItem("token") }
+        }
+      );
+
+      const parseRes = response.data;
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+
+    } catch (error: unknown) {
+      console.error(error instanceof Error ? error.message : 'An unknown error occurred');
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={!isAuthenticated ? <Landing /> : <Navigate to="/userHome" />} />
+        <Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to="/userHome" />}></Route>
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/userHome" />}></Route>
       </Routes>
     </Router>
   )
