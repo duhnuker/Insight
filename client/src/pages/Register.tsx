0651fb172d5b24885de2a6dd4169d09216700843
duhnuker <dinukanh@gmail.com>
2024-11-10 17:36:29 +1100

    User Registration onSubmitForm

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAuth }: { setAuth: (auth: boolean) => void }) => {
     const [inputs, setInputs] = useState({
         email: "",
         password: "",
@@ -14,11 +14,36 @@ const Register = () => {
         setInputs({ ...inputs, [e.target.name]: e.target.value });
     };
 
+    const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const body = { email, password, name };
+            const response = await axios.post("http://localhost:5000/auth/register",
+                body,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+
+            const parseRes = response.data;
+            localStorage.setItem("token", parseRes.jwtToken);
+            setAuth(true);
+
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                console.error(err.message);
+            } else {
+                console.error('An unknown error occurred');
+            }
+        }
+    };
+
     return (
         <div className="min-h-screen bg-gray-600 flex justify-center items-center">
             <div className="bg-gray-500 p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h1 className="text-3xl font-bold text-white mb-8 text-center">Register</h1>
-                <form className="space-y-6">
+                <form onSubmit={onSubmitForm} className="space-y-6">
                     <input
                         className="w-full px-4 py-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-transparent transition"
                         type='text'
